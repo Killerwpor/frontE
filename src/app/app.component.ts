12ecdb37155b910371392747841e103a76192464
Johan Suarez <johanc.suarez@hotmail.com>
2020-08-24 16:32:43 -0500

    proyecto base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import * as $ from "jquery";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'test';  
+  permitirScroll=false;
+  permitirMapa=false;
+ 
+  arrancarAnimacion(){
+    $(".ciudadEspejo").addClass("animate__animated animate__fadeOut");
+    setTimeout(()=>{
+      this.permitirScroll=true;
+      setTimeout(()=>{
+        this.permitirMapa=true;
+      }, 24500);
+    }, 1500);
+
+    
+  }
+
 }
