1669b8ab22f8d93848fe12b3464e0671a8fb7957
Johan Suarez <johanc.suarez@hotmail.com>
2020-09-03 10:59:20 -0500

    safe check antes de ultima versión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { Home2Component } from './home2/home2.component';
 import { AutoresComponent } from './autores/autores.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Home2Component,
     AutoresComponent
   ],
   imports: [
