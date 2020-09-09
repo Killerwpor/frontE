e9b62ca6a2b91a855701c52465d7c562484f3fad
Johan Suarez <johanc.suarez@hotmail.com>
2020-09-09 10:33:56 -0500

    safe commit

--- src/app/modelos/fragmentos.ts
+++ src/app/modelos/fragmentos.ts
@@ -9,7 +9,7 @@ import { Fragmento } from './fragmento';
         apellido: 'ARANGO',
         fuenteBibliografia: '"Medellín a solas contigo"',
         textoOriginal: "A veces apestas a gasolina y hollín, mi pequeña Detroit. ",
-        textoFragmento: "Tuve un sueño. Soñé con la plaza de Cisneros: incendiada. Con Coltejer: incendiado. Con el Parque Berrío: incendiado. Soñé con vos, Medellín, revestida toda en una llama eterna. Cada edificio. Cada calle.  Cada ser viviente. Una nube de humo, espesa y renegrida como el asfalto. Soñé que caminaba sobre la nube y te veía desde allá. Eras una boca infernal, mi querida Medellín. Y me lancé de cabeza para ser devorado por vos.",
+        textoFragmento: "Tuve un sueño. Soñé con la plaza de Cisneros: incendiada. Con Coltejer: incendiado. Con el Parque Berrío: incendiado. Soñé con vos, Medellín, plena en una llama eterna. Cada edificio. Cada calle.  Cada ser viviente. Una nube de humo, espesa y renegrida como el asfalto. Soñé que caminaba sobre la nube y te veía desde allá. Eras una boca infernal, mi querida Medellín. Y me lancé de cabeza para ser devorado por vos.",
         
     },
     {   
@@ -26,8 +26,8 @@ import { Fragmento } from './fragmento';
         nombre: 'JOSÉ MANUEL',
         apellido: 'ARANGO',
         fuenteBibliografia: '"El sueño rencoroso"',
-        textoOriginal: '"Es la ciudad tragada por la jungla. Uno puede oír el sordo rumor de raíces que crecen cuarteando los muros. "',
-        textoFragmento: 'En la aldea, esta es una historia que todos conocemos: más allá del bosque, viven todavía los primeros hombres. Han levantado una ciudad nueva sobre las cenizas de la vieja. Y ahora que la plaga arrasa con cada planta comestible, me han encomendado la tarea vital de confirmar lo que nos contaban los abuelos. Están allá abajo todos congregados. Han seguido los peligros de mi ascenso por el Gran Árbol con una alegría que creíamos olvidada para siempre. Veo un horizonte verde, potencialmente infinito. Veo un mundo bosque que ha crecido alrededor de toda humanidad posible, que nos abraza, que nos ahoga. Veo la luna, que asoma temprano. Pero no puedo ver la ciudad que había sido prometida. “¿Existe?”, gritan desde el suelo. Y hay en esos gritos la disposición desesperada de emprender el viaje que sea necesario. Respondo que sí entonces y no me arrepiento todavía. '
+        textoOriginal: 'Es la ciudad tragada por la jungla.\n Uno puede oír el sordo rumor de raíces que crecen cuarteando los muros.',
+        textoFragmento: 'Esta es una historia que todos conocemos en la aldea: más allá del bosque, viven todavía los primeros hombres. Han levantado una ciudad nueva sobre las cenizas de la vieja. Y ahora que la plaga arrasa con cada planta comestible, me han encomendado la tarea vital de confirmar lo que nos contaban los abuelos. Están allá abajo todos congregados. Han seguido mi ascenso por el Gran Árbol. Veo un horizonte verde, infinito. Veo un bosque que ha crecido alrededor de toda humanidad posible, que nos abraza, que nos ahoga. Pero no puedo ver la ciudad que había sido prometida. “¿Existe?”, gritan desde el suelo. Y hay en esos gritos la disposición desesperada de emprender el viaje que sea necesario. Respondo que sí entonces y no me arrepiento todavía. '
 
     },
     {
@@ -35,8 +35,8 @@ import { Fragmento } from './fragmento';
         nombre: 'JAIME',
         apellido: 'JARAMILLO ESCOBAR',
         fuenteBibliografia: '""En la luna"". En: Poemas para hoy. Editorial Universidad de Antioquia. Medellín, 2019."',
-        textoOriginal: '"Suelen decirme –a manera de crítica– que vivo en la Luna. ¿Les he dicho yo –a manera de crítica– que viven en Tierra?"',
-        textoFragmento: '"La nueva ciudad es un cráter a la medida de la vieja. Un ojo que mira a ese otro ojo opaco, ciego por las cataratas de la ruina. Los que pudimos escapar, construimos casas a imagen y semejanza de las casas de allá arriba. Replicamos las virtudes y defectos de la ciudad que abandonamos. Un edificio aguja. Calles estrechas. Un metro. Cavamos un río y partimos la ciudad en dos mitades. La asfixiamos deliberadamente con smog. Decimos: “Este es el mejor vividero del mundo, y queda en la Luna”. Fui pobre en la ciudad vieja y soy pobre en la ciudad nueva. Hago el mismo trabajo de ocho horas diarias, proceso información en ordenador que no va ya a ningún lado y le respondo a un jefe que simula también estar en algo productivo. Me llama la atención, de vez en cuando, porque no rindo, falsamente, lo que debería estar rindiendo. Tengo un gato idéntico al gato que perdí. Una mamá idéntica a la mamá que perdí. Una novia idéntica a la novia que perdí me abandona cada tanto. Fui un hombre nostálgico también en mi pasado y, aquí, desde la ingravidez de esta ciudad espejo, idéntica a la anterior hasta el detalle, miro hacia la Tierra y estoy obligado a entristecerme con un recuerdo innecesario, con el presentimiento de una pérdida que no puede ser nombrada."'
+        textoOriginal: 'Suelen decirme –a manera de crítica– que vivo en la Luna. ¿Les he dicho yo –a manera de crítica– que viven en Tierra?',
+        textoFragmento: 'La nueva ciudad es un cráter a la medida de la vieja. Los que pudimos escapar, construimos casas a imagen y semejanza de las casas de allá abajo. Un edificio aguja. Calles estrechas. Cavamos un río y partimos la ciudad en dos mitades. La asfixiamos deliberadamente con smog. Decimos: “Este es el mejor vividero del mundo, y queda en la Luna”. Fui pobre en la ciudad vieja y soy pobre en la nueva. Hago el mismo trabajo de ocho horas diarias. Tengo un gato idéntico al gato que perdí. Una novia idéntica a la novia que perdí me abandona cada tanto. Aquí, desde la ingravidez de esta ciudad, miro hacia la Tierra y me entristece un recuerdo innecesario, el presentimiento de una pérdida que no puede ser nombrada.'
 
     },
     {
@@ -44,8 +44,8 @@ import { Fragmento } from './fragmento';
         nombre: 'MANUELA',
         apellido: 'GÓMEZ',
         fuenteBibliografia: '"El cemento de una casa". En: La vida como era. Editorial Atarraya, 2017."',
-        textoOriginal: '"Parece de mentira el balcón y cuesta ver las plantas donde escondimos los soldaditos de plástico."',
-        textoFragmento: '"Oímos las últimas noticias y vimos luego avanzar el fuego hacia nosotros. Le dijimos adiós a todos los vecinos, que huían hacia no se sabe dónde, a morir en un lugar lejano. Decidimos refugiarnos en la casa. Nos dimos una ducha y nos pusimos la mejor ropa. La ropa de diciembre. Nos sentamos en la mesa. Rezamos. Comimos. Nos resignamos a la muerte, porque así nos habían enseñado. Cuando el humo se hizo insoportable, tomamos un sedante. Nos despedimos y fuimos a dormir y consideramos que sería para siempre. Dijimos: al menos nos amamos. Y sin embargo, despertamos. Abrimos las ventanas. Salimos al jardín intacto. La ciudad cercana era un desierto negro y la distante, un ardiente horizonte poco amenazante preñado de gritos diminutos, de opacas explosiones. Nos miramos. No hizo falta decirnos ya nunca nada: decidimos, en silencio, sembrar una huerta de tomates."'
+        textoOriginal: 'Parece de mentira el balcón\ny cuesta ver las plantas\ndonde escondimos\nlos soldaditos de plástico.',
+        textoFragmento: 'Oímos las últimas noticias y vimos avanzar el fuego hacia nosotros. Le dijimos adiós a todos los vecinos, que huían a morir en un lugar lejano. Decidimos refugiarnos en la casa. Nos dimos una ducha y nos pusimos la mejor ropa. Nos sentamos en la mesa. Rezamos. Comimos. Cuando el humo se hizo insoportable, tomamos un sedante. Nos despedimos y fuimos a dormir y consideramos que sería para siempre. Dijimos: al menos nos amamos. Y sin embargo, despertamos. Abrimos las ventanas. Salimos al jardín intacto. La ciudad cercana era un desierto negro y la distante, un ardiente horizonte poco amenazante preñado de gritos diminutos, de opacas explosiones. Nos miramos. No hizo falta decirnos ya nunca nada: decidimos, en silencio, sembrar una huerta de tomates.'
 
     },
     {
@@ -53,8 +53,8 @@ import { Fragmento } from './fragmento';
         nombre: '"YENNY',
         apellido: 'LEÓN',
         fuenteBibliografia: 'De Entre árboles y piedras. Planeta, 2013."',
-        textoOriginal: '"la niña se hunde en el cuarto silencio más largo de la tierra"',
-        textoFragmento: '"El día que descendí al pozo cumplí ocho años. La tía Mariana me dio pastel de chocolate. Y helado de chocolate. Y me regaló un vestido nuevo. Y me dijo: “Vamos”. Y vinimos. Entonces sucedió el incendio. Dejamos atrás un montón de cosas chamuscadas. Y mi tía no parecía tener miedo ni nada. Entonces bajamos al pozo. Y me dijo: “Espera acá”. Y yo espero. He esperado mucho. Cada día, arriba, en la boca del pozo, un circulito de luz se abre y alguien, que no es mi tía, porque no me habla, y mi tía hablaba mucho conmigo, baja la comida con un balde. Yo no odio a mi tía, porque me dio todo desde que mis papás murieron. Tengo miedo por ella, más bien. A veces, en esta oscuridad, creo oír voces acá abajo. Ayer, por ejemplo, una voz pequeñita, pequeñita, dijo: “Hablar era cosa del pasado”. Y yo le dije: “¡Oigan a este! Si yo con usted no estoy hablando”. Y ya no dijo nada ni yo tampoco, porque es mejor no hablar con gente que no existe. "'
+        textoOriginal: 'la niña se hunde\nen el cuarto silencio más largo de la tierra',
+        textoFragmento: 'El día que descendí al pozo cumplí ocho años. La tía Mariana me dio pastel de chocolate. Y helado de chocolate. Y me regaló un vestido nuevo. Y me dijo: “Vamos”. Entonces bajamos al pozo y sucedió el incendio. Y mi tía no parecía tener miedo ni nada. Y me dijo: “Espera acá”. Y yo espero. He esperado mucho. Cada día, arriba, en la boca del pozo, un circulito de luz se abre y alguien, que no es mi tía, baja la comida con un balde. Yo no odio a mi tía, porque me dio todo desde que mis papás murieron. Tengo miedo por ella, más bien. A veces, en esta oscuridad, creo oír voces acá abajo. Ayer, por ejemplo, una voz pequeñita, pequeñita, dijo: “Hablar era cosa del pasado”. Y yo le dije: “¡Oigan a este! Si yo con usted no estoy hablando”. Y ya no dijo nada ni yo tampoco, porque es mejor no hablar con gente que no existe.'
 
     },
     {
@@ -62,8 +62,8 @@ import { Fragmento } from './fragmento';
         nombre: 'JORGE MARIO',
         apellido:  'BETANCUR',
         fuenteBibliografia: 'Moscas de todos los colres: barrio Guayaquil de Medellín, 1894-1034. Editorial Universidad de Antioquia. Medellín, 2006',
-        textoOriginal: 'Sin luz eléctrica, las sombras en las calles se volvían tenebrosas. Fantasmas y seres de otro mundo se tomaban la población. Pero no había que temer, todos estaban en casa. ',
-        textoFragmento: 'Voy cada fin de semana a visitar al fantasma de mi hija. El viaje a través del valle me toma un día entero si lo hago caminando, pero puedo ir en tres horas si Carmen me presta su caballo. Donde estaba antes la casa de mi hija —en la que vivía con su madre—, ahora solo hay escombros cenicientos y una espesa vegetación que se enmaraña a sus columnas y que nadie sospechaba que fuera posible después de la noche de todos los incendios. Calculo bastante bien, a pesar de los años, de la ruina, cada espacio de la casa.  La recorro y la invoco, digo su nombre: “Yadira”, y espero que aparezca. A veces no tengo suerte y cuando la tengo, su imagen dura apenas dos o tres segundos. La he visto crecer todos estos años. Volverse adulta. Asumo que visita al fantasma de su madre, cada fin de semana también, porque ella ha creado una vida-muerte independiente. Con un esposo fantasma, imagino. Hijos fantasmas. Perro fantasma acaso.'
+        textoOriginal: 'Sin luz eléctrica, las sombras en las calles se volvían tenebrosas.\n Fantasmas y seres de otro mundo se tomaban la población. Pero no había que temer, todos estaban en casa. ',
+        textoFragmento: 'Voy cada fin de semana a visitar al fantasma de mi hija. El viaje a través del valle me toma un día entero si lo hago caminando, pero puedo ir en tres horas si me prestan un caballo. Donde estaba antes nuestra casa, ahora solo hay escombros cenicientos y una espesa vegetación que nadie sospechaba que fuera posible después de la noche de todos los incendios. Calculo bastante bien, a pesar de los años, de la ruina, cada espacio de la casa. La recorro y la invoco, digo su nombre, "Yadira", y espero que aparezca. Cuando tengo suerte, su imagen dura apenas unos segundos. La he visto crecer todos estos años, volverse adulta. Porque ella tiene una vida en la muerte, desde luego: tiene un esposo fantasma, hijos fantasmas y perro fantasma acaso.'
 
     },
     {
@@ -71,7 +71,7 @@ import { Fragmento } from './fragmento';
         nombre: 'MANUEL',
         apellido: 'MEJÍA VALLEJO',
         fuenteBibliografia:'Aire de tango. Editorial Eafit. Medellín, 2008. "',
-        textoOriginal: '"Aquí vino a morir Gardel en llamaradas, y en ellas el nacimiento del mito, su voz prodigiosa al fondo, ésa que escuchaba día y noche el barrio de Guayaquil en las madrugadas."',
+        textoOriginal: 'Aquí vino a morir Gardel en llamaradas, y en ellas el nacimiento del mito, su voz prodigiosa al fondo, ésa que escuchaba día y noche el barrio de Guayaquil en las madrugadas.',
         textoFragmento: '"Murió Gardel por el fuego y, como había sido prometido, revivió por el fuego también. Emergió de las llamas en la noche de todos los incendios. Ahí estábamos sus fieles esperando, en el exacto lugar en que lo habíamos perdido. Y su silueta, revestida de fuego, cantó para nosotros una última vez: Yo adivino el parpadeo de las luces que a lo lejos van marcando mi retorno. Seguimos la canción con la voluntad inquebrantable de aquel que está cumplliendo su destino. Juramos soportar lo que fuera necesario. Incluso la muerte. Cayeron mis amigos, uno a uno, en un último grito fulgurante. Nadie estuvo en pie para saber que no  pude  oír la estrofa final, que no fui capaz de hacerlo, que me arrepentí en el último minuto. Ahora solo puedo imaginarla en este rincón de la deshonra: Vivir con el alma aferrada a un dulce recuerdo que lloro otra vez."'
 
     },
