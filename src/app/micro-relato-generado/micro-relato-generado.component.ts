2b6c96d8aaa86b0784ac356b491d6361bd261145
Johan Suarez <johanc.suarez@hotmail.com>
2020-09-18 16:48:34 -0500

    safe commit

--- src/app/micro-relato-generado/micro-relato-generado.component.ts
+++ src/app/micro-relato-generado/micro-relato-generado.component.ts
@@ -25,21 +25,32 @@ export class MicroRelatoGeneradoComponent implements OnInit {
 
  compartir: boolean=false;
  editar: boolean=false;
+ compartirRedes: boolean=false;
 
   constructor(public microRelatoServicio: MicrorelatosService) { }
 
   ngOnInit(): void {
-    
+    this.porcentajeSolarPunk=""
   }
 
   clickEditar(){
      this.editar=true;
   }
 
+  habilitarCompartir(){
+    this.compartirRedes=true;
+  }
+
   clickOtro(){   
      this.microRelatoServicio.generarMicroRelato(this.respuestas).subscribe(result => {
        this.textoGenerado=result.texto;
       });
+  }
+
+  async guardarS3(img){
+    //alert("guardar");
+    var datos= this.microRelatoServicio.uploadFile(img);
+    return datos;
   }
 
   guardarMicroRelato(){
@@ -53,27 +64,81 @@ export class MicroRelatoGeneradoComponent implements OnInit {
      });
   }
 
-  guardarImagen(){
-    var node = document.getElementById('negativo');
-
-domtoimage.toPng(node)
+ async compartirFb(){
+ $(".container-fluid2").removeClass("hide");
+    var node = document.getElementById('test');
+    var img = new Image();
+await domtoimage.toPng(node)
     .then(function (dataUrl) {
-        var img = new Image();
+       
         img.src = dataUrl;
-        document.getElementById('imagenNueva').appendChild(img);
+        $(".container-fluid2").addClass("hide");
+      //  document.getElementById('imagenNueva').appendChild(img);
     })
+    
     .catch(function (error) {
       alert("error");
-        console.error('oops, something went wrong!', error);
-    });
+        console.error('oops, something went wrong!', error);    });
+
+    
+    var timestamp = new Date().getTime().toString(); //esto es para tener un nombre unico de archivo
+    var file = this.dataURLtoFile(img.src, timestamp);
+   var url=this.guardarS3(file);
+   console.log("https://imagenes-generadas.s3.amazonaws.com/"+timestamp);
+    this.compartirFacebook("https://imagenes-generadas.s3.amazonaws.com/"+timestamp);
+    
   }
+
+  async compartirTw(){
+    $(".container-fluid2").removeClass("hide");
+       var node = document.getElementById('test');
+       var img = new Image();
+   await domtoimage.toPng(node)
+       .then(function (dataUrl) {
+          
+           img.src = dataUrl;
+           $(".container-fluid2").addClass("hide");
+         //  document.getElementById('imagenNueva').appendChild(img);
+       })
+       
+       .catch(function (error) {
+         alert("error");
+           console.error('oops, something went wrong!', error);    });
+   
+       
+       var timestamp = new Date().getTime().toString(); //esto es para tener un nombre unico de archivo
+       var file = this.dataURLtoFile(img.src, timestamp);
+      var url=this.guardarS3(file);
+      console.log("https://imagenes-generadas.s3.amazonaws.com/"+timestamp);
+       this.compartirTwitter("https://imagenes-generadas.s3.amazonaws.com/"+timestamp);
+       
+     }
   
 
-  compartirFacebook(TheImg) {
-  var u="https://w7.pngwing.com/pngs/582/403/png-transparent-spongebob-squarepants-and-patrick-star-against-blue-background-patrick-star-cartoon-drawing-desktop-rey-cartoon-food-fictional-character-spongebob.png"
+  compartirFacebook(url) {
    // t=document.title;
   var t="descripcion";
-  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;
+  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(url)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;
+  //window.open('https://www.facebook.com/sharer.php?u=www.google.com&picture=https://media.revistagq.com/photos/5ca5f6a77a3aec0df5496c59/16:9/w_1920,c_limit/bob_esponja_9564.png', 'ventanacompartir', 'toolbar=0, status=0, width=650, height=450');
 }
+
+compartirTwitter(url) {
+  // t=document.title;
+ var t="descripcion";
+ window.open("https://twitter.com/share?url="+encodeURIComponent(url)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;
+ //window.open('https://www.facebook.com/sharer.php?u=www.google.com&picture=https://media.revistagq.com/photos/5ca5f6a77a3aec0df5496c59/16:9/w_1920,c_limit/bob_esponja_9564.png', 'ventanacompartir', 'toolbar=0, status=0, width=650, height=450');
+}
+
+ dataURLtoFile(dataurl, filename) {
+  var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+      while(n--){
+          u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new File([u8arr], filename, {type:mime});
+  }
+
+
+  
 
 }
--- src/app/microrelatos/microrelatos.service.ts
+++ src/app/microrelatos/microrelatos.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,13 +13,13 @@ export class MicrorelatosService {
   constructor(private http: HttpClient) { }
 
 
-  // private urlGenerar: string = "http://127.0.0.1:3000/api/microRelatos/generarMicroRelato";
-  // private urlGuardar: string = "http://127.0.0.1:3000/api/microRelatos/guardarMicroRelato";
-  // private urlTraer: string = "http://127.0.0.1:3000/api/microRelatos/traerMicroRelatos";
+  private urlGenerar: string = "http://127.0.0.1:3000/api/microRelatos/generarMicroRelato";
+  private urlGuardar: string = "http://127.0.0.1:3000/api/microRelatos/guardarMicroRelato";
+  private urlTraer: string = "http://127.0.0.1:3000/api/microRelatos/traerMicroRelatos";
 
-  private urlGenerar: string = "http://ec2-18-220-134-88.us-east-2.compute.amazonaws.com/api/api/microRelatos/generarMicroRelato";
-  private urlGuardar: string = "http://ec2-18-220-134-88.us-east-2.compute.amazonaws.com/api/api/microRelatos/guardarMicroRelato";
-  private urlTraer: string = "http://ec2-18-220-134-88.us-east-2.compute.amazonaws.com/api/api/microRelatos/traerMicroRelatos";
+  // private urlGenerar: string = "http://ec2-18-220-134-88.us-east-2.compute.amazonaws.com/api/api/microRelatos/generarMicroRelato";
+  // private urlGuardar: string = "http://ec2-18-220-134-88.us-east-2.compute.amazonaws.com/api/api/microRelatos/guardarMicroRelato";
+  // private urlTraer: string = "http://ec2-18-220-134-88.us-east-2.compute.amazonaws.com/api/api/microRelatos/traerMicroRelatos";
 
 
   private httpHeaders = new HttpHeaders({
@@ -47,6 +50,34 @@ export class MicrorelatosService {
   traerMicroRelatos(): Observable<any> {       
         return this.http.get<any>(this.urlTraer);    
   }
+
+async uploadFile(file) {
+    var respuesta;
+    const contentType = file.type;
+    const bucket = new S3(
+          {
+              accessKeyId: 'AKIA2Z4IXMV3F4PYBGGX',
+              secretAccessKey: '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',
+              region: 'us-east-1'
+          }
+      );
+      const params = {
+          Bucket: 'imagenes-generadas',
+          Key: file.name,
+          Body: file,
+          ACL: 'public-read',
+          ContentType: contentType
+      };
+    await  bucket.upload(params, function (err, data) {
+          if (err) {
+              console.log('There was an error uploading your file: ', err);
+              return false;
+          }
+          console.log('Successfully uploaded file.', data);
+        respuesta=data;
+      });
+      return respuesta;
+}
 
 
 }
