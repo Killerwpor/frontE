c907dcb6b7f4acb58d0334dfbbc09c413623ef5e
Johan Suarez <johanc.suarez@hotmail.com>
2020-09-28 00:32:51 -0500

    movil horizontal terminada

--- src/app/creditos/creditos.component.ts
+++ src/app/creditos/creditos.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-creditos',
@@ -6,12 +6,24 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   styleUrls: ['./creditos.component.css']
 })
 export class CreditosComponent implements OnInit {
+  orientacion: String;
   movil: Boolean=false;
   @Output() navegador = new EventEmitter<string>();
 
   constructor(private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
+
+    if (window.outerWidth > window.outerHeight) {
+      this.orientacion="landscape";
+      
+  
+  }
+  else{
+    this.orientacion="portrait";
+  }
+
+
     this.movil= this.deviceService.isMobile();
   }
 
@@ -31,6 +43,19 @@ export class CreditosComponent implements OnInit {
           break;     
     }
     
+  }
+
+  @HostListener('window:orientationchange', ['$event'])
+  onOrientationChange(event) {
+    if (window.outerWidth > window.outerHeight) {
+      this.orientacion="portrait";
+  
+  }
+  else{
+   
+    this.orientacion="landscape";
+  }
+  //alert(this.orientacion);
   }
 
 }
